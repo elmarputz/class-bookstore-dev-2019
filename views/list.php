66378dc0ac8b70f2282a7ae8aeb16e54f7fcc659
Elmar Putz <ep@studiomitte.com>
2019-05-11 11:14:33 +0200

    [TPL] list

--- views/list.php
+++ views/list.php
@@ -1,23 +1,43 @@
-<?php 
+<?php
+
 use Data\DataManager;
+use Bookshop\Util;
+
 $categories = DataManager::getCategories();
 $categoryId = (int) $_REQUEST['categoryId'] ?? null;
-$books = (isset($categoryId) && ($categoryId > 0)) ? 
-    DataManager::getBooksByCategory($categoryId) : null;
-var_dump($books);
+$books = (isset($categoryId) && ($categoryId > 0)) ? DataManager::getBooksByCategory($categoryId) : null;
 
+require_once('views/partials/header.php');
+?>
 
-require_once('views/partials/header.php'); ?>
+<div class="page-header">
+    <h2>List of books by category</h2>
+</div>
 
 <ul class="nav nav-tabs">
-    <?php foreach ($categories as $cat) { ?>
+    <?php foreach ($categories as $cat) : ?>
         <li role="presentation"
-            <?php if ($cat->getId() === $categoryId) { ?> class="active" <?php } ?>
-        >
-            <a href="<?php echo $_SERVER['PHP_SELF']; ?>?view=list&categoryId=<?php echo urlencode($cat->getId()); ?>"><?php echo $cat->getName(); ?></a> 
+            <?php if ($cat->getId() === $categoryId) { ?>class="active" <?php } ?>>
+            <a href="<?php echo $_SERVER['PHP_SELF'] ?>?view=list&categoryId=<?php echo urlencode($cat->getId()); ?>"><?php $cat->getName(); ?></a></span>
         </li>
-    <?php } ?>
+    <?php endforeach; ?>
 </ul>
 
+<br />
 
-<?php require_once('views/partials/footer.php'); ?>
+<?php if (isset($books)) : ?>
+  <?php
+  if (sizeof($books) > 0) :
+    require('views/partials/booklist.php');
+  else :
+    ?>
+
+    <div class="alert alert-warning" role="alert">No books in this category.</div>
+  <?php endif; ?>
+<?php else : ?>
+  <div class="alert alert-info" role="alert">Please select a category.</div>
+<?php endif; ?>
+
+
+
+<?php require_once('views/partials/footer.php');
