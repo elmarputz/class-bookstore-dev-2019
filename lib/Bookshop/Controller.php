7f17c0182ebb2eac5e23f388ba6122ac31c8eadd
Elmar Putz <ep@studiomitte.com>
2019-05-25 10:35:43 +0200

    [TPL] forwardRequest

--- lib/Bookshop/Controller.php
+++ lib/Bookshop/Controller.php
@@ -65,4 +65,26 @@ class Controller extends BaseObject {
 
     }    
 
+
+     /**
+   * 
+   * @param array $errors : optional assign it to 
+   * @param string $target : url for redirect of the request
+   */
+  protected function forwardRequest(array $errors = null, $target = null) {
+    //check for given target and try to fall back to previous page if needed
+    if ($target == null) {
+      if (!isset($_REQUEST[self::PAGE])) {
+        throw new Exception('Missing target for forward.');
+      }
+      $target = $_REQUEST[self::PAGE];
+    }
+    //forward request to target
+    // optional - add errors to redirect and process them in view
+    if (count($errors) > 0)
+      $target .= '&errors=' . urlencode(serialize($errors));
+    header('location: ' . $target);
+    exit();
+  }
+
 }
