4a962d2d4983c309d9d2f2f12773c6a2e1daa55a
Elmar Putz <ep@studiomitte.com>
2019-06-15 15:13:52 +0200

    [TPL] DataManager user methods

--- lib/Data/DataManager_mysqli.php
+++ lib/Data/DataManager_mysqli.php
@@ -98,13 +98,121 @@ class DataManager implements IDataManager {
 
   }
 
-  public static function getUserById (int $userId) {
-     
- 
-  }
-  public static function getUserByUserName(string $userName) {
-   
-  }
+  /**
+	 * get the books per search term
+	 *
+	 * note: search via LIKE
+	 *
+	 * @param string $term search term: book title string match
+	 *
+	 * @return array of Book-items
+	 */
+	public static function getBooksForSearchCriteria($term) {
+		$books = array();
+		$con   = self::getConnection();
+		$term  = $con->real_escape_string($term); /* !!! */
+		$res   = self::query($con, "
+      SELECT id, categoryId, title, author, price 
+      FROM books 
+      WHERE title LIKE '%" . $term . "%';
+            ");
+		while ($book = self::fetchObject($res)) {
+			$books[] = new Book($book->id, $book->categoryId, $book->title, $book->author, $book->price);
+		}
+		self::close($res);
+		self::closeConnection($con);
+
+		return $books;
+	}
+
+	/**
+	 * get the books per search term – paginated set only
+	 *
+	 * @param string  $term       search term: book title string match
+	 * @param integer $offset     start at the nth item
+	 * @param integer $numPerPage number of items per page
+	 *
+	 * @return array of Book-items
+	 */
+	public static function getBooksForSearchCriteriaWithPaging($term, $offset, $numPerPage) {
+		$con = self::getConnection();
+		//query total count
+		$term       = $con->real_escape_string($term); /* !!! */
+		$res        = self::query($con, "
+      SELECT COUNT(*) AS cnt 
+      FROM books 
+      WHERE title LIKE '%" . $term . "%';
+        ");
+		$totalCount = self::fetchObject($res)->cnt;
+		self::close($res);
+		//query books to return
+		$books      = array();
+		$offset     = intval($offset); /* !!! */
+		$numPerPage = intval($numPerPage); /* !!! */
+		$res        = self::query($con, "
+      SELECT id, categoryId, title, author, price 
+      FROM books 
+      WHERE title LIKE '%" . $term . "%' 
+      LIMIT " . $offset . ", " . $numPerPage . ";
+        ");
+		while ($book = self::fetchObject($res)) {
+			$books[] = new Book($book->id, $book->categoryId, $book->title, $book->author, $book->price);
+		}
+		self::close($res);
+		self::closeConnection($con);
+
+		return new PagingResult($books, $offset, $totalCount);
+	}
+
+	/**
+	 * get the User item by id
+	 *
+	 * @param integer $userId uid of that user
+	 *
+	 * @return User | false
+	 */
+	public static function getUserById(int $userId) {
+		$user   = null;
+		$con    = self::getConnection();
+		$userId = intval($userId); /* !!! */
+		$res    = self::query($con, "
+      SELECT id, userName, passwordHash 
+      FROM users
+      WHERE id = " . $userId . ";
+        ");
+		if ($u = self::fetchObject($res)) {
+			$user = new User($u->id, $u->userName, $u->passwordHash);
+		}
+		self::close($res);
+		self::closeConnection($con);
+
+		return $user;
+	}
+
+	/**
+	 * get the User item by name
+	 *
+	 * @param string $userName name of that user - must be exact match
+	 *
+	 * @return User | false
+	 */
+	public static function getUserByUserName(string $userName) {
+		$user     = null;
+		$con      = self::getConnection();
+		$userName = $con->real_escape_string($userName); /* !!! */
+		$res      = self::query($con, "
+      SELECT id, userName, passwordHash 
+      FROM users 
+      WHERE userName = '" . $userName . "';
+        ");
+		if ($u = self::fetchObject($res)) {
+			$user = new User($u->id, $u->userName, $u->passwordHash);
+		}
+		self::close($res);
+		self::closeConnection($con);
+
+		return $user;
+	}
 
   public static function createOrder (int $userId, array $bookIds, 
       string $nameOnCard, string $cardNumber) : int {
